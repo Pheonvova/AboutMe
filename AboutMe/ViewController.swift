83b41ec70118eb47c05302883439e5e703c8c495
Pheonvova <pheon1310@gmail.com>
2020-06-10 17:20:52 +0300

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Pheon on 10.06.2020.
+//  Copyright © 2020 Pheon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
